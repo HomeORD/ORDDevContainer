689582fecc05383d7e0ab628d9248e62f7aa3476
Mutharayappa <gururaj.mutharayappa@accenture.com>
2018-01-18 14:25:02 +0530

    Demo project committed

--- demo/src/main/java/com/example/demo/WebRestController.java
+++ demo/src/main/java/com/example/demo/WebRestController.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController 
-public class WebRestController {
-	 @RequestMapping("/api/hi")  
+@RestController
+public class WebRestController implements ErrorController{
+	 
+	private static final String PATH="/error";
+	@RequestMapping("/api/hi")  
 	    public String hi(){  
 	        return"Hello Guru from rest API!";  
 	    }
+	
+	@RequestMapping(value = PATH)  
+    public String defaultErrorMessage(){  
+        return "Requested resource not found";  
+    }
+
+
+	@Override
+	public String getErrorPath() {
+		// TODO Auto-generated method stub
+		return PATH;
+	}
+	 
 
 }
