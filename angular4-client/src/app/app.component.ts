8cd0ecbe6fced10ad7308830f5f01abb99faa246
Tn <jayanti.tn@accenture.com>
2018-03-12 19:25:36 +0530

    App Component Changes

--- angular4-client/src/app/app.component.ts
+++ angular4-client/src/app/app.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component, HostListener, ElementRef   } from '@angular/core';
 import {
   trigger,
@@ -7,25 +6,10 @@ import {
   animate,
   transition  
 }from '@angular/animations';
-=======
-import { Component } from '@angular/core';
-import { HostListener, ElementRef } from '@angular/core';
-import { 
-   trigger, 
-   state, 
-   style, 
-   animate, 
-   transition 
- } from '@angular/animations';
- 
- import {ContainerComponent} from './container/container.component';
->>>>>>> f87f4b0509ff3ac33e9d92ab55f792b9c7d70f35
 
 @Component({
-  template: `<container [sections]="sections"></container>`,
   selector: 'app-root',
   templateUrl: './app.component.html',
-<<<<<<< HEAD
   styleUrls: ['./app.component.css'],  
   animations: [
     trigger('scrollAnimation', [
@@ -40,30 +24,27 @@ import {
       transition('show => hide', animate('700ms ease-out')),
       transition('hide => show', animate('700ms ease-in'))
     ])
-=======
-  styleUrls: ['./app.component.css'],
-    animations: [ 
-    trigger('scrollAnimation', [ 
-      state('show', style({ 
-        opacity: 1, 
-        transform: "translateX(0)" 
-      })), 
-      state('hide',   style({ 
-        opacity: 0, 
-        transform: "translateX(-100%)" 
-      })), 
-      transition('show => hide', animate('700ms ease-out')), 
-      transition('hide => show', animate('700ms ease-in')) 
-    ]) 
->>>>>>> f87f4b0509ff3ac33e9d92ab55f792b9c7d70f35
   ]
 })
-
 export class AppComponent {
   title = 'Tour of Heroes';
   state = 'hide';
   
-<<<<<<< HEAD
+  public show:boolean = false;
+  public buttonName:any = 'Menu';
+  
+  ngOnInit () {  }
+
+  toggle() {
+    this.show = !this.show;
+
+    // CHANGE THE NAME OF THE BUTTON.
+    if(this.show)  
+      this.buttonName = "Close";
+    else
+      this.buttonName = "Menu";
+  }
+  
   public sections = [
   {name:"Bird",img:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Hummingbird.jpg/320px-Hummingbird.jpg"},
   {name:"Wheel", img:"https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/The_OC_Fair_ferris_wheel.jpg/320px-The_OC_Fair_ferris_wheel.jpg"},
@@ -85,27 +66,4 @@ export class AppComponent {
       }
 
     }
-=======
-    public sections = [
-  {name:"Bird",img:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Hummingbird.jpg/320px-Hummingbird.jpg"},
-  {name:"Wheel", img:"https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/The_OC_Fair_ferris_wheel.jpg/320px-The_OC_Fair_ferris_wheel.jpg"},
-  {name:"Horses", img:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/D%C3%BClmen%2C_Merfeld%2C_D%C3%BClmener_Wildpferde_in_der_Wildbahn_--_2016_--_4740.jpg/320px-D%C3%BClmen%2C_Merfeld%2C_D%C3%BClmener_Wildpferde_in_der_Wildbahn_--_2016_--_4740.jpg"}];
-
-  constructor(public el: ElementRef) { } 
-
-
-
-  @HostListener('window:scroll', ['$event']) 
-  checkScroll() { 
-    const componentPosition = this.el.nativeElement.offsetTop 
-    const scrollPosition = window.pageYOffset 
-
-
-    if (scrollPosition >= componentPosition) { 
-      this.state = 'show' 
-    } else { 
-      this.state = 'hide' 
-    } 
-  } 
->>>>>>> f87f4b0509ff3ac33e9d92ab55f792b9c7d70f35
 }
--- angular4-client/src/app/app.module.ts
+++ angular4-client/src/app/app.module.ts
@@ -5,13 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-<<<<<<< HEAD
+
 import { StickyNavModule } from 'ng2-sticky-nav';
 import { NgStickyDirective } from 'ng-sticky';
 import {DropdownModule} from "ng2-dropdown";
-=======
-import {ContainerComponent} from './container/container.component';
->>>>>>> f87f4b0509ff3ac33e9d92ab55f792b9c7d70f35
 
 @NgModule({
   declarations: [
@@ -22,13 +19,9 @@ import {ContainerComponent} from './container/container.component';
   imports: [
     BrowserModule,
     FormsModule,
-<<<<<<< HEAD
     BrowserAnimationsModule,
     StickyNavModule,
     DropdownModule
-=======
-    BrowserAnimationsModule    
->>>>>>> f87f4b0509ff3ac33e9d92ab55f792b9c7d70f35
   ],
   providers: [],
   bootstrap: [AppComponent]
